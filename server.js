5a860b15c70823122a5700105cea074eace66f90
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-01-24 17:27:21 -0700

    connect server to database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[enviornment];
-// const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[enviornment];
+const database = require('knex')(configuration);
 
 const express = require('express')
 const app = express()
